533f5cecfc97134902f6138488b3abe9f2bc23b8
John Henry Liwag <93307570+johnSantol@users.noreply.github.com>
2022-12-08 10:25:37 +0000

    Use ajax to query mysqli

--- scan.php
+++ scan.php
@@ -1,49 +1,193 @@
-<!doctype html>
-<html lang="en">
-  <head>
-   <meta charset="utf-8">
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Team JuanBayabas">
-    <meta name="LMS" content="LMS">
-    <title>Library Management System</title>
-
-<script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-
-
-    <link href="navbar-top-fixed.css" rel="stylesheet">
-  </head>
-  <body>
- 
-    <form action="JSON.php" method="POST">
-    <!-- <form action="JSON.php" name ="submit_form" method="post"> //auto submit -->
-
-    <video id="preview" style="border-radius:0% ;margin-left:0px ;width: 500px; height: 400px;" ></video>
-       
-    <!-- <form action=""  method="POST"> -->
-<!-- get JWT_Token from scanned qrcode then parse it -->
-    <input  name="JWT_Token" id="JWT_Token">
-    <input  name="srcode" id="srcode">
-    <input  name="fullname" id="fullname">
-    <input  name="timestamp" id="timestamp">
-    <input  name="type" id="type">
-    <input  name="userid" id="userid">
-    <input class="btn login_btn" type="submit" name="submit_form"/>
-<!-- End of parsing -->
-
-</form>
-        
+    <meta name="robots" content="noindex,nofollow">
+    <title>Library Monitoring System</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
+    <!-- chartist CSS -->
+    <link href="assets/plugins/chartist-js/dist/chartist.min.css" rel="stylesheet">
+    <link href="assets/plugins/chartist-js/dist/chartist-init.css" rel="stylesheet">
+    <link href="assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css" rel="stylesheet">
+    <!--This page css - Morris CSS -->
+    <link href="assets/plugins/c3-master/c3.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.min.css" rel="stylesheet">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+</head>
 
-    
-  
-<!-- </form> -->
-</div>
+<body>
+   
+ <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+      <header class="topbar" data-navbarbg="skin6">
+            <nav class="navbar top-navbar navbar-expand-md navbar-dark">
+                <div class="navbar-header" data-logobg="">
+                    <a class="navbar-brand ms-3 nav-link dropdown-toggle text-white " href="main.php"><span class="logo-text">Library Monitoring System</span>
+                    </a>
+                </div>
+                <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
+                    <ul class="navbar-nav d-lg-none d-md-block ">
+                        <li class="nav-item">
+                            <a class="nav-toggler nav-link waves-effect waves-light text-white "href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav me-auto mt-md-0 ">
+                      
+                        <li class="nav-item search-box">
+                            
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="assets/images/users/1.png" alt="administrator" class="profile-pic me-2">Administrator
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown"></ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <aside class="left-sidebar" data-sidebarbg="skin6">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <!-- User Profile-->
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="main.php" aria-expanded="false"><i class="mdi me-2 mdi-gauge"></i><span
+                                    class="hide-menu">Dashboard</span></a></li>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="scan.php" aria-expanded="false">
+                                <i class="mdi me-2 mdi-account-check"></i><span class="hide-menu">Scan QR</span></a>
+                        </li>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="table-basic.html" aria-expanded="false"><i class="mdi me-2 mdi-table"></i><span
+                                    class="hide-menu">Schedule</span></a></li>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="icon-material.html" aria-expanded="false"><i
+                                    class="mdi me-2 mdi-emoticon"></i><span class="hide-menu">Logs</span></a></li>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="map-google.html" aria-expanded="false"><i class="mdi me-2 mdi-earth"></i><span
+                                    class="hide-menu">Blank</span></a></li>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="ShowStudents.php" aria-expanded="false"><i
+                                    class="mdi me-2 mdi-book-open-variant"></i><span class="hide-menu">Vacancy</span></a>
+                        </li>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="pages-error-404.html" aria-expanded="false"><i class="mdi me-2 mdi-help-circle"></i><span
+                                    class="hide-menu">Error 404</span></a>
+                        </li>
+                       <!--  <li class="text-center p-20 upgrade-btn">
+                            <a href="https://www.wrappixel.com/templates/materialpro/"
+                                class="btn btn-warning text-white mt-4" target="_blank">Upgrade to
+                                Pro</a>
+                        </li> -->
+                    </ul>
 
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+            <div class="sidebar-footer">
+                <div class="row">
+                    <div class="col-4 link-wrap">
+                        <!-- item-->
+                        <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Settings"><i
+                                class="ti-settings"></i></a>
+                    </div>
+                    <div class="col-4 link-wrap">
+                        <!-- item-->
+                        <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i
+                                class="mdi mdi-gmail"></i></a>
+                    </div>
+                    <div class="col-4 link-wrap">
+                        <!-- item-->
+                        <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i
+                                class="mdi mdi-power"></i></a>
+                    </div>
+                </div>
+            </div>
+        </aside>
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row align-items-center">
+                    <div class="col-md-6 col-8 align-self-center">
+                        <h3 class="page-title mb-0 p-0">Scan QR</h3>
+                        <div class="d-flex align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Scan QR</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-4 align-self-center">
+                        <div class="text-end upgrade-btn">
+                            <!-- <a href="https://www.wrappixel.com/templates/materialpro/"
+                                class="btn btn-danger d-none d-md-inline-block text-white" target="_blank">Upgrade to
+                                Pro</a> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <form id="fupForm" name="form1" method="post">
+                                    <video id="preview" style="border-radius:0% ;margin-left:0px ;width: 500px; height: 400px;" ></video>
+                                    <input  hidden name="JWT_Token" id="JWT_Token">
+                                    <input  hidden name="srcode" id="srcode">
+                                    <input  hidden name="fullname" id="fullname">
+                                    <input  hidden name="timestamp" id="timestamp">
+                                    <input  hidden name="type" id="type">
+                                    <input  hidden name="userid" id="userid">
 
-</main>
+                                    <label id="timestamp_label"></label>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 <script>
+
+
 let b64DecodeUnicode = str => decodeURIComponent(Array.prototype.map.call(atob(str), c =>'%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join(''))
 
 let parseJwt = token =>JSON.parse(b64DecodeUnicode(token.split('.')[1].replace('-', '+').replace('_', '/')))
@@ -62,55 +206,97 @@ Instascan.Camera.getCameras().then(function(cameras){
            scanner.addListener('scan',function(c){
           
                // document.getElementById('JWT_Token').value=c;
-               var jwt = c;
-               
-             var code = JSON.stringify(parseJwt(jwt)) 
-             
-             
-             
-             
-             
-             
-             // hindi magrereload ang web site dine, automatic na are in the background. check mo nalang ang console atsaka network sa developer option kung nagana nga
-             var myHeaders = new Headers();
-              myHeaders.append("Content-Type", "application/json");
-
-
-              var requestOptions = {
-                method: 'POST',
-                headers: myHeaders,
-                body: code,
-                redirect: 'follow'
-              };
-
-              fetch("/json.php", requestOptions)
-                .then(response => response.text())
-                .then(result => console.log(result))
-                .catch(error => console.log('error', error));
-             
-             
-             
-             
-             
-             
-             
-               
+                // var jwt = $('JWT_Token')
+                var srcode = $('#srcode').val();
+                var fullname = $('#fullname').val();
+                var timestamp = $('#timestamp').val();
+                var type = $('#type').val();
+                var userid = $('#userid').val();
+                var jwt = c;
+                var code = JSON.stringify(parseJwt(jwt))
                 document.getElementById('JWT_Token').value = code;
                 var obj = JSON.parse(code);
+                var srcode = obj.srcode;
+                var fullname = obj.fullname;
+                var timestamp = obj.timestamp;
+                var type = obj.type;
+                var userid = obj.userid;
                 document.getElementById('srcode').value = obj.srcode;
                 document.getElementById('fullname').value = obj.fullname;
                 document.getElementById('timestamp').value = obj.timestamp;
                 document.getElementById('type').value = obj.type;
                 document.getElementById('userid').value = obj.userid;
+                document.getElementById('timestamp_label').value = obj.timestamp;
+
+                $.ajax({
+        url: "JSON.php",
+        type: "POST",
+        data: {
+            srcode: srcode,
+            fullname: fullname
+                    
+        },
+        cache: false,
+        success: function(dataResult){
+            var dataResult = JSON.parse(dataResult);
+            // if(dataResult.statusCode==200){
+                                    
+            // }
+            // else if(dataResult.statusCode==201){
+            //     alert("Error occured !");
+            // }
+            
+        }
+    });
                 alert('QrDetected');
-                // document.submit_form.submit();   //auto submit ng form pagkatapos mascan yung qr code para di na kelangan magclick ng button para masubmit
-                // e.preventDefault();
+
            });
-           
-           // alert(code);
-        </script>
-      
-  </body>
+
+
+</script>
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <footer class="footer"> © 2021 Material Pro Admin by <a href="https://www.wrappixel.com/">wrappixel.com </a>
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="../assets/plugins/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../assets/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/app-style-switcher.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.js"></script>
+</body>
+
 </html>
-
-
